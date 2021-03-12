bf15448c3e1ca5c3d24ef69d07f374b537028c56
fransotodev <franciscomanuel.sotoramirez@gmail.com>
2021-03-12 18:09:22 +0100

    New rounds for POA and a script to execute them all

---
+++ scripts/executePOAfolder.js
@@ -0,0 +1,26 @@
+//Not using this script, it probably overlap
+
+const fs = require("fs");
+const { execSync } = require("child_process");
+
+const listConfigs = fs.readdirSync(
+  `${__dirname}/../caliperWorkspace/benchmarks/config_POA_benchmarks`
+);
+
+listConfigs.map(async (configFile, index) => {
+  console.log(index);
+  listenerFile = {};
+  listenerFile[
+    `${configFile}`
+  ] = execSync(
+    `npx caliper launch manager --caliper-workspace ${__dirname}/../caliperWorkspace/ --caliper-benchconfig benchmarks/config_POA_benchmarks/${configFile} --caliper-networkconfig networks/networkConfigGeth.json`,
+    { stdio: "inherit" }
+  );
+  fs.copyFileSync(
+    `${__dirname}/../caliperWorkspace/report.html`,
+    `${__dirname}/../caliperWorkspace/benchmarks/results_POA_benchmarks/report${
+      index + 1
+    }.html`
+  );
+  return null;
+});
